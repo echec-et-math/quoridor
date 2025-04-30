6ed3b57a9ade3d7ad0a3fbf4903840231fcc7590
Ulysse DUPONT-LATAPIE <ulysseh2o@free.fr>
2025-04-30 15:23:12 +0200

    added file uploading

--- webserver.py
+++ webserver.py
@@ -8,11 +8,6 @@ headers =  {"Content-Type":"application/json"}
 
 app = Flask(__name__)
 
-default_score_response = [
-    {'opponent' : 'alice', 'victory' : True},
-    {'opponent' : 'bob', 'victory' : False}
-]
-
 default_score_response2 = [
     {'opponent' : 'test', 'victory' : True},
     {'opponent' : 'test2', 'victory' : False}
@@ -20,8 +15,8 @@ default_score_response2 = [
 
 @app.get("/")
 def website_root():
-
-    return 200
+    resp = make_response("Welcome to the Quoridor web server ! Please register at /register, submit your code at /submitcode and check your scores at /score !")
+    return resp, 200
 
 @app.get("/score")
 def get_score():
@@ -30,14 +25,34 @@ def get_score():
     # fetch scores from datasheet
     # build response in JSON
     if cookie == None:
-        return jsonify(default_score_response) # TODO
+        resp = make_response("You are not registered to the server yet !")
+        return resp, 401
     else:
+        resp = make_response()
         return jsonify(default_score_response2)
 
+@app.get("/submitcode")
+def submitcode_get():
+    resp = make_response("""<form action="submitcode" method="post" enctype=multipart/form-data>
+    Code file : <input id="fileupload" name="submission" type="file" />
+    <input type="submit" value="Submit">
+</form>""")
+    return resp, 200
+
 @app.post("/submitcode")
-def submit_code():
-    resp = make_response("Not implemented yet.")
-    return jsonify(resp), 500
+def submitcode_post():
+    cookie = request.cookies.get("quoridor_user_id")
+    if cookie == None:
+        resp = make_response("You are not registered to the server yet !")
+        return resp, 401
+    submission = request.files.get("submission")
+    if submission == None:
+        resp = make_response("No file sent.")
+        return resp, 400
+    submission.save("submissions/" + cookie + ".py")
+    resp = make_response("Sucessfully saved your code.")
+    return resp, 201
+
 
 @app.get("/register")
 def create_cookie_id():
@@ -46,12 +61,13 @@ def create_cookie_id():
     # add cookie to jar
     cookie = request.cookies.get("quoridor_user_id")
     if cookie == None:
-        resp = make_response("Successfully registered.")
-        resp.set_cookie("quoridor_user_id", hex(rd.randint(1, 2**128)))
-        return resp, 200
+        id = hex(rd.randint(1, 2**32))
+        resp = make_response("Successfully registered as : " + str(id))
+        resp.set_cookie("quoridor_user_id", id)
+        return resp, 201
     else:
         # user already exists
-        resp = make_response("You are already registered !")
+        resp = make_response("You are already registered as " + str(cookie) + " !")
         return resp, 403
 
 @app.get("/unregister")
@@ -60,7 +76,7 @@ def unregister():
     if cookie != None:
         resp = make_response("Successfully unregistered.")
         resp.delete_cookie("quoridor_user_id")
-        return resp, 200
+        return resp, 201
     else:
         # user doesn't exist
         resp = make_response("You never registered before !")
